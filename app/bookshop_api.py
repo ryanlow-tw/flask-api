e358c16285c114a25b1440316ab2a1c185a79d8f
ryanlow-tw <ryan.low@thoughtworks.com>
2021-07-13 11:05:01 +0800

    add route to retrieve database info

--- app/bookshop_api.py
+++ app/bookshop_api.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import logging
+import sqlite3
 
+db_path = "../database/books.db"
 app = Flask(__name__)
 logging.basicConfig(filename="../log.txt", level=logging.INFO)
 
@@ -15,6 +17,20 @@ def index():
 def hello():
     logging.info("This is the hello page.")
     return "Hello World"
+
+
+@app.route('/books')
+def books():
+    query = "SELECT * FROM books_50"
+    return get_data(query)
+
+
+def get_data(query):
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute(query)
+        json_results = [dict((cursor.description[i][0], value) for i, value in enumerate(row)) for row in cursor.fetchall()]
+        return {"result": json_results}
 
 
 if __name__ == '__main__':
