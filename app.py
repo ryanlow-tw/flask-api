1fb671cbe462ecadb2c20072a73b5c5ee552ff8d
ryanlow-tw <ryan.low@thoughtworks.com>
2021-07-15 02:28:26 +0800

    add query strings for book route to order by price

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
-import logging
 from database.booksdb import Database
 from db_parser.db_parser import parse_data
+from sqlalchemy import text
+import logging
 
 db_path = 'sqlite:///database/books.db'
 app = Flask(__name__)
@@ -28,6 +29,13 @@ def hello():
     return "Hello World"
 
 
+@app.route('/books/<int:book_id>', methods=["GET"])
+def books_id(book_id):
+    table_results = db.session.query(bookshop.db).filter_by(id=book_id).all()
+    data = parse_data(table_results)
+    return data
+
+
 @app.route('/books/all', methods=["GET"])
 def all_books():
     table_results = db.session.query(bookshop.db).all()
@@ -37,14 +45,13 @@ def all_books():
 
 @app.route('/books', methods=["GET"])
 def books():
-    table_results = db.session.query(bookshop.db).all()
-    data = parse_data(table_results)
-    return data
+    table_results = None
+    order = request.args.get('order')
+    if order.lower() == "desc":
+        table_results = db.session.query(bookshop.db).order_by(bookshop.db.c.price.desc()).all()
+    elif order.lower() == "asc":
+        table_results = db.session.query(bookshop.db).order_by(bookshop.db.c.price).all()
 
-
-@app.route('/books/<int:book_id>', methods=["GET"])
-def books_id(book_id):
-    table_results = db.session.query(bookshop.db).filter_by(id=book_id).all()
     data = parse_data(table_results)
     return data
 
