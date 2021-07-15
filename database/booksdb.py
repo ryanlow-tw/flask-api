79cc5cad547730f0c02bb1e7c965688263c1358d
ryanlow-tw <ryan.low@thoughtworks.com>
2021-07-15 12:22:07 +0800

    refactor code into multiple lines

--- database/booksdb.py
+++ database/booksdb.py
@@ -17,4 +17,7 @@ class Database:
             self.table_name = table_name
 
     def load_db(self):
-        self.db = self.db.Table(self.table_name, self.db.metadata, autoload=True, autoload_with=self.db.engine)
+        self.db = self.db.Table(self.table_name,
+                                self.db.metadata,
+                                autoload=True,
+                                autoload_with=self.db.engine)
