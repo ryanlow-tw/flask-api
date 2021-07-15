b3aa1c04185999019efb1de35b194b1613452749
ryanlow-tw <ryan.low@thoughtworks.com>
2021-07-15 11:46:29 +0800

    add test for api query

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,11 +3,12 @@ import requests
 from app import index, hello
 from unittest.mock import patch
 from database.booksdb import Database
+import json
 
 
 class TestBookShop_API(unittest.TestCase):
     API_URL = "http://127.0.0.1:5000"  # extract into config file
-
+    maxDiff = 2000
     def test_root_page_returns_string_index_page(self):
         INDEX_ROUTE = "/"
         ENDPOINT_URL = f"{self.API_URL}{INDEX_ROUTE}"
@@ -41,3 +42,53 @@ class TestBookShop_API(unittest.TestCase):
         exception = error.exception
         self.assertEqual(f"{exception}", "Database cannot be instantiated more than once :(")
 
+    def test_should_return_books_containing_row(self):
+        INDEX_ROUTE = "/books?name=mey"
+        ENDPOINT_URL = f"{self.API_URL}{INDEX_ROUTE}"
+        r = requests.get(ENDPOINT_URL)
+
+        json_object = json.loads(r.text)
+        author_name = json_object['results'][0]['author'].lower()
+
+        self.assertEqual(200, r.status_code)
+        self.assertIn("mey", author_name)
+
+    def test_should_return_books_containing_exact_price(self):
+        INDEX_ROUTE = "/books?price=3409"
+        ENDPOINT_URL = f"{self.API_URL}{INDEX_ROUTE}"
+        r = requests.get(ENDPOINT_URL)
+
+        json_object = json.loads(r.text)
+        price = int(json_object['results'][0]['price'])
+        self.assertEqual(200, r.status_code)
+        self.assertEqual(3409, price)
+
+    def test_should_return_books_containing_language_code(self):
+        INDEX_ROUTE = "/books?language=en"
+        ENDPOINT_URL = f"{self.API_URL}{INDEX_ROUTE}"
+
+        r = requests.get(ENDPOINT_URL)
+        json_object = json.loads(r.text)
+        language = json_object['results'][0]['language_code'].lower()
+        self.assertEqual(200, r.status_code)
+        self.assertIn("en", language)
+
+    def test_should_return_books_containing_isbn(self):
+        INDEX_ROUTE = "/books?isbn=316"
+        ENDPOINT_URL = f"{self.API_URL}{INDEX_ROUTE}"
+
+        r = requests.get(ENDPOINT_URL)
+        json_object = json.loads(r.text)
+        isbn = json_object['results'][0]['isbn']
+        self.assertEqual(200, r.status_code)
+        self.assertIn("316", isbn)
+
+    def test_should_return_books_containing_isbn13(self):
+        INDEX_ROUTE = "/books?isbn13=978"
+        ENDPOINT_URL = f"{self.API_URL}{INDEX_ROUTE}"
+
+        r = requests.get(ENDPOINT_URL)
+        json_object = json.loads(r.text)
+        isbn = json_object['results'][0]['isbn13']
+        self.assertEqual(200, r.status_code)
+        self.assertIn("978", isbn)
