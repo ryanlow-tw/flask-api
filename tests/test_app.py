ffcbcc399d80f2ac91a0b738f5369fe0006cc8ef
ryanlow-tw <ryan.low@thoughtworks.com>
2021-07-16 11:57:32 +0800

    edit test to run without starting server

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,29 +1,25 @@
 import unittest
-import requests
+import app
 from app import index, hello
 from unittest.mock import patch
 from database.booksdb import Database
-import json
 
 
 class TestBookShop_API(unittest.TestCase):
-    API_URL = "http://127.0.0.1:5000"  # extract into config file
+
+    def setUp(self):
+        self.app = app.app.test_client()
+        self.app.testing = True
 
     def test_root_page_returns_string_index_page(self):
-        INDEX_ROUTE = "/"
-        ENDPOINT_URL = f"{self.API_URL}{INDEX_ROUTE}"
-        r = requests.get(ENDPOINT_URL)
-
+        r = self.app.get('/')
         self.assertEqual(200, r.status_code)
-        self.assertEqual("Index Page", r.text)
+        self.assertEqual(b"Index Page", r.get_data())
 
     def test_hello_route_returns_string_index_page(self):
-        INDEX_ROUTE = "/hello"
-        ENDPOINT_URL = f"{self.API_URL}{INDEX_ROUTE}"
-        r = requests.get(ENDPOINT_URL)
-
+        r = self.app.get('hello')
         self.assertEqual(200, r.status_code)
-        self.assertEqual("Hello World", r.text)
+        self.assertEqual(b"Hello World", r.get_data())
 
     @patch("app.logging.info")
     def test_that_root_page_request_should_be_logged(self, info_mock):
@@ -42,52 +38,43 @@ class TestBookShop_API(unittest.TestCase):
         self.assertEqual(f"{exception}", "Database cannot be instantiated more than once :(")
 
     def test_should_return_books_containing_row(self):
-        INDEX_ROUTE = "/books?name=mey"
-        ENDPOINT_URL = f"{self.API_URL}{INDEX_ROUTE}"
-        r = requests.get(ENDPOINT_URL)
 
-        json_object = json.loads(r.text)
+        r = self.app.get('/books?name=mey')
+        json_object = r.json
         author_name = json_object['results'][0]['author'].lower()
 
         self.assertEqual(200, r.status_code)
         self.assertIn("mey", author_name)
 
     def test_should_return_books_containing_exact_price(self):
-        INDEX_ROUTE = "/books?price=3409"
-        ENDPOINT_URL = f"{self.API_URL}{INDEX_ROUTE}"
-        r = requests.get(ENDPOINT_URL)
 
-        json_object = json.loads(r.text)
+        r = self.app.get("/books?price=3409")
+        json_object = r.json
         price = int(json_object['results'][0]['price'])
+
         self.assertEqual(200, r.status_code)
         self.assertEqual(3409, price)
 
     def test_should_return_books_containing_language_code(self):
-        INDEX_ROUTE = "/books?language=en"
-        ENDPOINT_URL = f"{self.API_URL}{INDEX_ROUTE}"
+        r = self.app.get("/books?language=en")
 
-        r = requests.get(ENDPOINT_URL)
-        json_object = json.loads(r.text)
+        json_object = r.json
         language = json_object['results'][0]['language_code'].lower()
         self.assertEqual(200, r.status_code)
         self.assertIn("en", language)
 
     def test_should_return_books_containing_isbn(self):
-        INDEX_ROUTE = "/books?isbn=316"
-        ENDPOINT_URL = f"{self.API_URL}{INDEX_ROUTE}"
+        r = self.app.get("/books?isbn=316")
 
-        r = requests.get(ENDPOINT_URL)
-        json_object = json.loads(r.text)
+        json_object = r.json
         isbn = json_object['results'][0]['isbn']
         self.assertEqual(200, r.status_code)
         self.assertIn("316", isbn)
 
     def test_should_return_books_containing_isbn13(self):
-        INDEX_ROUTE = "/books?isbn13=978"
-        ENDPOINT_URL = f"{self.API_URL}{INDEX_ROUTE}"
 
-        r = requests.get(ENDPOINT_URL)
-        json_object = json.loads(r.text)
+        r = self.app.get("/books?isbn13=978")
+        json_object = r.json
         isbn = json_object['results'][0]['isbn13']
         self.assertEqual(200, r.status_code)
         self.assertIn("978", isbn)
