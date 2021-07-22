833b7c2e23a0d800452eaae69e45b4a370109d37
ryanlow-tw <ryan.low@thoughtworks.com>
2021-07-22 10:27:02 +0800

    change database uri to docker postgres

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 class Config(object):
     pass
 
-
 class EnvironmentConfig(Config):
+    username = 'postgres'
+    database_name = username
+    password = 'mysecretpassword'
+    port = 5432
+
     FLASK_ENV = "dev"
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database/books.db'
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{username}:{password}@postgres:{port}/{database_name}'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
